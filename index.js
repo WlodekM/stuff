24e6da4d87fe7db41c3c773560888f14d40c0778
WlodekM <wlodzio.maly@gmail.com>
2023-03-20 20:19:38 +0200

    a

---
+++ index.js
@@ -0,0 +1,27 @@
+import CloudlinkClient from '@williamhorning/cloudlink';
+
+// setup a new client
+let cloudlink = new CloudlinkClient({
+  url: "wss://server.meower.org/",
+  log: true
+})
+
+// listen for packets
+cloudlink.on("packet", (packet) => {
+  console.log("Received packet:", packet)
+})
+
+// send a packet
+async function sendtest() {
+    await cloudlink.send({
+      cmd: "direct",
+      val: {
+        "cmd": "gen_account",
+        "val": {
+            "username": "testing1212",
+            "pswd": "pass"
+        }
+      },
+      listener: "join"
+    })
+}
---
+++ index.svelte
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<script>
+import CloudlinkClient from 'cl.js';
+
+// setup a new client
+let cloudlink = new CloudlinkClient({
+  url: "wss://server.meower.org/",
+  log: true
+})
+
+// listen for packets
+cloudlink.on("packet", (packet) => {
+  console.log("Received packet:", packet)
+})
+
+// send a packet
+async function sendtest() {
+    await cloudlink.send({
+      cmd: "direct",
+      val: {
+        "cmd": "gen_account",
+        "val": {
+            "username": "testing1212",
+            "pswd": "pass"
+        }
+      },
+      listener: "join"
+    })
+}
+</script>
+<button onclick="sendtest()">TEST!</button>
